93fff0fee26acb43181d79fa3e470f27c6a5cf62
ezequiel.martinez <ezequiel.martinez@mrm-mccann.com>
2020-12-02 21:10:59 -0300

    revisar Paginator

--- spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/datajpa/app/models/dao/IClienteDao.java
+++ spring-boot-data-jpa/src/main/java/com/bolsadeideas/springboot/datajpa/app/models/dao/IClienteDao.java
@@ -1,10 +1,10 @@
 package com.bolsadeideas.springboot.datajpa.app.models.dao;
  
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.bolsadeideas.springboot.datajpa.app.models.entity.Cliente;
 
-public interface IClienteDao extends CrudRepository<Cliente, Long> {
+public interface IClienteDao extends PagingAndSortingRepository<Cliente, Long> {
 	
 
 }
